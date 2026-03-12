846400ef68b22d11180f2f4775cfab77e5b1c146
lxeu <wangmatthew6000@gmail.com>
2026-03-11 18:48:35 -0600

    update coffee machine files

--- Day16/coffee_machine/main.py
+++ Day16/coffee_machine/main.py
@@ -1,3 +1,21 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+coffee_machine_on = True
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+menu = Menu()
+
+while coffee_machine_on:
+    coffee = input("What would you like? (espresso/latte/cappuccino): ")
+    if coffee == "report":
+        coffee_maker.report()
+        money_machine.report()
+    elif coffee == "off":
+        coffee_machine_on = False
+    else: 
+        drink = menu.find_drink(coffee)
+        if drink and coffee_maker.is_resource_sufficient(drink):
+            payment_successful = money_machine.make_payment(drink.cost)
+            if payment_successful:
+                coffee_maker.make_coffee(drink)
