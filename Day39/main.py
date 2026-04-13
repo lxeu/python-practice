7b1f6279f72ab526a42ff3ac7736e70541d7b62d
lxeu <wangmatthew6000@gmail.com>
2026-04-13 13:43:40 -0600

    update day 39

--- Day39/main.py
+++ Day39/main.py
@@ -26,6 +26,7 @@ ORIGIN_CITY_IATA = "YEG"
 
 for destination in sheet_data:
     pprint(f"Getting flights for {destination['city']}...")
+
     flights = flight_search.check_flights(
         ORIGIN_CITY_IATA,
         destination["iataCode"],
@@ -33,14 +34,24 @@ for destination in sheet_data:
         to_time=six_month_from_today
     )
 
-    cheapest_flight = find_cheapest_flight(flights, return_date=six_month_from_today.strftime("%Y-%m-%d"))
+    cheapest_flight = find_cheapest_flight(
+        flights,
+        return_date=six_month_from_today.strftime("%Y-%m-%d")
+    )
+
     pprint(f"{destination['city']}: CAD {cheapest_flight.price}")
 
-    if cheapest_flight.price != "N/A" and cheapest_flight.price < destination["lowestPrice"]:
+    if isinstance(cheapest_flight.price, (int, float)) and cheapest_flight.price < destination["lowestPrice"]:
         print(
-            f"Lower price flight found to {destination['city']}! "
-            f"From {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
-            f"Airline: {airline}\n"
-            f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}"
+            f"DEAL FOUND!!\n"
+            f"{destination['city']} - CAD {cheapest_flight.price}\n"
+            f"{cheapest_flight.origin_airport} → {cheapest_flight.destination_airport}\n"
+            f"Airline: {cheapest_flight.airline}\n"
+            f"Flight #: {cheapest_flight.flight_number}\n"
+            f"{cheapest_flight.out_date} → {cheapest_flight.return_date}\n"
         )
-        data_manager.update_lowest_price(destination["id"], cheapest_flight.price)
+
+        data_manager.update_lowest_price(
+            destination["id"],
+            cheapest_flight.price
+        )
